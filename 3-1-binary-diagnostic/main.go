79346ace2f9fd453850d4127a9854a97ff73295d
paulschick <paulschick19@gmail.com>
2022-05-09 14:36:08 -0400

    3.1 - rune to int example

--- 3-1-binary-diagnostic/main.go
+++ 3-1-binary-diagnostic/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
+func ReadLines() []string {
+  content, err := ioutil.ReadFile("input.txt")
+  if err != nil {
+    log.Fatal(err)
+  }
+  linesString := string(content)
+  lines := strings.Split(linesString, "\n")
+  return lines
+}
+
 func main() {
-  fmt.Println("hi")
+  lines := ReadLines()
+  fmt.Println(lines[0])
+  for _, character := range lines[0] {
+    buf := make([]byte, 1)
+    _ = utf8.EncodeRune(buf, character)
+    value, _ := strconv.Atoi(string(buf))
+    fmt.Println(value)
+    fmt.Printf("%T\n", value)
+  }
 }
