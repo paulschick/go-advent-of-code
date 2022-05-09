22237052c26bc28ea8f3aa60e5cbdcc540e84400
paulschick <paulschick19@gmail.com>
2022-05-09 19:41:21 -0400

    3.1 - get gamma, epsilon numbers

--- 3-1-binary-diagnostic/main.go
+++ 3-1-binary-diagnostic/main.go
@@ -19,16 +19,23 @@ func ReadLines() []string {
   return lines
 }
 
-// Deprecated: following MostCommonNumber, this will be replaced with
-// a conversion from a byte, not a rune
-// func IntFromRune(character rune) int64 {
-//   buf := make([]byte, 1) 
-//   _ = utf8.EncodeRune(buf, character)
-//   value, _ := strconv.Atoi(string(buf))
-//   return int64(value)
-// }
+func gammaCompare(countOnes int, countZeros int) bool {
+  if countOnes >= countZeros {
+    return true
+  } else {
+    return false
+  }
+}
 
-func MostCommonNumber(lines []string) []int64 {
+func epsilonCompare(countOnes int, countZeros int) bool {
+  if countOnes < countZeros {
+    return true
+  } else {
+    return false
+  }
+}
+
+func GetGamma(lines []string) []int64 {
   gamma := []int64 {}
 
   trimmed := strings.TrimSpace(lines[0])
@@ -48,9 +55,7 @@ func MostCommonNumber(lines []string) []int64 {
       }
     }
 
-    if countOnes > countZeros {
-      gamma = append(gamma, int64(1))
-    } else if countOnes == countZeros {
+    if gammaCompare(countOnes, countZeros) {
       gamma = append(gamma, int64(1))
     } else {
       gamma = append(gamma, int64(0))
@@ -59,7 +64,39 @@ func MostCommonNumber(lines []string) []int64 {
   return gamma
 }
 
+func GetEpsilon(lines []string) []int64 {
+  epsilon := []int64 {}
+
+  trimmed := strings.TrimSpace(lines[0])
+  colCount := len(trimmed)
+
+  for colIndex := 0; colIndex < colCount; colIndex++ {
+    countOnes := 0
+    countZeros := 0
+
+    for _, row := range lines {
+      valInt, _ := strconv.Atoi(string(row[colIndex]))
+
+      if valInt == 0 {
+        countZeros += 1
+      } else if valInt == 1 {
+        countOnes += 1
+      }
+    }
+
+    if epsilonCompare(countOnes, countZeros) {
+      epsilon = append(epsilon, int64(1))
+    } else {
+      epsilon = append(epsilon, int64(0))
+    }
+  }
+  return epsilon
+}
+
 func main() {
-  gamma := MostCommonNumber(ReadLines())
+  lines := ReadLines()
+  gamma := GetGamma(lines)
   fmt.Println("Gamma: ", gamma)
+  epsilon := GetEpsilon(lines)
+  fmt.Println("Epsilon: ", epsilon)
 }
