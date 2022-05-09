756bf1fb5ee2361fdf55347f1bdeb22f43620fb4
paulschick <paulschick19@gmail.com>
2022-05-09 18:30:01 -0400

    3.1 - Gets gamma values

--- 3-1-binary-diagnostic/main.go
+++ 3-1-binary-diagnostic/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"strconv"
 	"strings"
-	"unicode/utf8"
+	// "unicode/utf8"
 )
 
 func ReadLines() []string {
@@ -19,14 +19,47 @@ func ReadLines() []string {
   return lines
 }
 
-func main() {
-  lines := ReadLines()
-  fmt.Println(lines[0])
-  for _, character := range lines[0] {
-    buf := make([]byte, 1)
-    _ = utf8.EncodeRune(buf, character)
-    value, _ := strconv.Atoi(string(buf))
-    fmt.Println(value)
-    fmt.Printf("%T\n", value)
+// Deprecated: following MostCommonNumber, this will be replaced with
+// a conversion from a byte, not a rune
+// func IntFromRune(character rune) int64 {
+//   buf := make([]byte, 1) 
+//   _ = utf8.EncodeRune(buf, character)
+//   value, _ := strconv.Atoi(string(buf))
+//   return int64(value)
+// }
+
+func MostCommonNumber(lines []string) []int64 {
+  gamma := []int64 {}
+
+  trimmed := strings.TrimSpace(lines[0])
+  colCount := len(trimmed)
+
+  for colIndex := 0; colIndex < colCount; colIndex++ {
+    countOnes := 0
+    countZeros := 0
+
+    for _, row := range lines {
+      valInt, _ := strconv.Atoi(string(row[colIndex]))
+
+      if valInt == 0 {
+        countZeros += 1
+      } else if valInt == 1 {
+        countOnes += 1
+      }
+    }
+
+    if countOnes > countZeros {
+      gamma = append(gamma, int64(1))
+    } else if countOnes == countZeros {
+      gamma = append(gamma, int64(1))
+    } else {
+      gamma = append(gamma, int64(0))
+    }
   }
+  return gamma
+}
+
+func main() {
+  gamma := MostCommonNumber(ReadLines())
+  fmt.Println("Gamma: ", gamma)
 }
